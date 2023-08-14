6fccbe6db971b84cc69db7c98141476755ce234d
AnubhavTwo7 <anubhav.pal@research.iiit.ac.in>
2023-08-14 16:49:18 +0530

    updated color blindness

--- experiment/references.md
+++ experiment/references.md
@@ -3,3 +3,5 @@
 2) https://www.nei.nih.gov/learn-about-eye-health/eye-conditions-and-diseases/color-blindness 
 3) https://usabilitygeek.com/colors-in-ui-design-a-guide-for-creating-the-perfect-ui/ 
 4) https://enchroma.com/pages/types-of-color-blindness 
+5) Wong, B. (2011). Points of view: Color blindness. In Nature Methods (Vol. 8, Issue 6, pp. 441–441). Springer Science and Business Media LLC. https://doi.org/10.1038/nmeth.1618
+6) Judd, D. B. (1943). Facts of Color-Blindness*. In Journal of the Optical Society of America (Vol. 33, Issue 6, p. 294). The Optical Society. https://doi.org/10.1364/josa.33.000294
--- experiment/simulation/js/cb_main.js
+++ experiment/simulation/js/cb_main.js
@@ -1,60 +1,145 @@
-function protonopia() {
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
-  var image = document.getElementById("img");
-  ctx.drawImage(image, 0, 0);
-  var imgData = ctx.getImageData(0, 0, c.width, c.height);
+let currentImageSrc = null;
+let currentColorBlindness = null;
+let originalImageData = null;
 
-    for (var i = 0; i < imgData.data.length; i += 4) {
-        imgData.data[i] = 0;
-        imgData.data[i + 1] = imgData.data[i + 1];
-        imgData.data[i + 2] = imgData.data[i + 2];
-        imgData.data[i + 3] = 255;
+function showImage(src) {
+  const canvas = document.getElementById('imageCanvas');
+  const context = canvas.getContext('2d');
+  const image = new Image();
+
+  const thumbnails = document.getElementsByClassName('image-thumbnail');
+  for (let i = 0; i < thumbnails.length; i++) {
+    thumbnails[i].style.display = 'none';
+  }
+
+  image.onload = function() {
+    // Calculate the aspect ratio to maintain image proportions
+    const maxWidth = 400; // You can adjust this value to control the maximum width of the scaled-down image
+    const maxHeight = 300; // You can adjust this value to control the maximum height of the scaled-down image
+    let width = image.width;
+    let height = image.height;
+
+    if (width > maxWidth) {
+      height *= maxWidth / width;
+      width = maxWidth;
     }
-    ctx.putImageData(imgData, 0, 0);
-}
 
-function tritanopia() {
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    var image = document.getElementById("img");
-    ctx.drawImage(image, 0, 0);
-    var imgData = ctx.getImageData(0, 0, c.width, c.height);
-
-    for (var i = 0; i < imgData.data.length; i += 4) {
-        imgData.data[i] = imgData.data[i];
-        imgData.data[i + 1] = imgData.data[i + 1];
-        imgData.data[i + 2] = 0;
-        imgData.data[i + 3] = 255;
+    if (height > maxHeight) {
+      width *= maxHeight / height;
+      height = maxHeight;
     }
-    ctx.putImageData(imgData, 0, 0);
+
+    canvas.width = width;
+    canvas.height = height;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(image, 0, 0, width, height);
+    document.getElementById('canvas').style.display = 'block';
+    currentImageSrc = src;
+    originalImageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    simulateColorBlindness();
+  };
+
+  image.src = src;
 }
 
-function totalcolorblind() {
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    var image = document.getElementById("img");
-    ctx.drawImage(image, 0, 0);
-    var imgData = ctx.getImageData(0, 0, c.width, c.height);
+function simulateColorBlindness() {
+  if (currentImageSrc) {
+    const type = currentColorBlindness || 'normal';
 
-    for (var i = 0; i < imgData.data.length; i += 4) {
-        imgData.data[i] =
-            0.3 * imgData.data[i] +
-            0.59 * imgData.data[i + 1] +
-            0.11 * imgData.data[i + 2];
-        imgData.data[i + 1] =
-            0.3 * imgData.data[i] +
-            0.59 * imgData.data[i + 1] +
-            0.11 * imgData.data[i + 2];
-        imgData.data[i + 2] =
-            0.3 * imgData.data[i] +
-            0.59 * imgData.data[i + 1] +
-            0.11 * imgData.data[i + 2];
-        imgData.data[i + 3] = 255;
+    const canvas = document.getElementById('imageCanvas');
+    const context = canvas.getContext('2d');
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.putImageData(originalImageData, 0, 0);
+
+    const image = new Image();
+    image.onload = function() {
+      const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+      const simulatedData = simulateImageData(imageData, type);
+
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.putImageData(simulatedData, 0, 0);
+    };
+
+    image.src = currentImageSrc;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const thumbnails = document.getElementsByClassName('image-thumbnail');
+  for (let i = 0; i < thumbnails.length; i++) {
+    thumbnails[i].addEventListener('click', function() {
+      showImage(this.src);
+      currentColorBlindness = null; // Reset the color blindness type when switching images
+    });
+  }
+
+  const protonopiaBtn = document.getElementById('protonopiaBtn');
+  protonopiaBtn.addEventListener('click', function() {
+    currentColorBlindness = 'protonopia';
+    simulateColorBlindness();
+  });
+
+  const tritanopiaBtn = document.getElementById('tritanopiaBtn');
+  tritanopiaBtn.addEventListener('click', function() {
+    currentColorBlindness = 'tritanopia';
+    simulateColorBlindness();
+  });
+
+  const colorblindBtn = document.getElementById('colorblindBtn');
+  colorblindBtn.addEventListener('click', function() {
+    if (currentColorBlindness === 'colorblind') {
+      currentColorBlindness = null; // Reset the color blindness type when clicking the same button
+    } else {
+      currentColorBlindness = 'colorblind';
     }
-    ctx.putImageData(imgData, 0, 0);
-}
+    simulateColorBlindness();
+  });
 
-function endExperiment() {
-    writeText("Thanks for participating!");
-}
+    const imageFile = document.getElementById('imageFile');
+  imageFile.addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function(readerEvent) {
+      const img = new Image();
+      img.onload = function() {
+        showImage(img.src);
+      };
+      img.src = readerEvent.target.result;
+    };
+
+    reader.readAsDataURL(file);
+  });
+});
+
+function simulateImageData(imageData, type) {
+  const data = imageData.data;
+  const length = data.length;
+
+  for (let i = 0; i < length; i += 4) {
+    const red = data[i];
+    const green = data[i + 1];
+    const blue = data[i + 2];
+
+    if (type === 'protonopia') {
+      // Simulate protonopia (red-green color blindness)
+      const grayscale = 0.2989 * red + 0.587 * green + 0.114 * blue;
+      data[i] = grayscale;
+      data[i + 1] = grayscale;
+    } else if (type === 'tritanopia') {
+      // Simulate tritanopia (blue-yellow color blindness)
+      const grayscale = 0.2989 * red + 0.587 * green + 0.114 * blue;
+      data[i + 2] = grayscale;
+    } else if (type === 'colorblind') {
+      // Simulate general color blindness
+      const grayscale = 0.2989 * red + 0.587 * green + 0.114 * blue;
+      data[i] = grayscale;
+      data[i + 1] = grayscale;
+      data[i + 2] = grayscale;
+    }
+  }
+
+  return imageData;
+} 
+  
